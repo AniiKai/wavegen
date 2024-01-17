e65587ff6a0d2dc51d98b016902e8950c1d25211
Bladen Sawatsky <BKSawatsky@gmail.com>
2024-01-16 22:43:45 -0800

    changed ascii values and added fps

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ void printScr(double** A, int h, int l) {
 	for (int i=0; i<h; i++) {
 		for (int j=0; j<l; j++) {
 			if (A[i][j] <= -3.5) printf(".");
-			else if (A[i][j] <= -3.0) printf(":");
-			else if (A[i][j] <= -2.5) printf("-");
+			else if (A[i][j] <= -3.0) printf("-");
+			else if (A[i][j] <= -2.5) printf(":");
 			else if (A[i][j] <= -2.0) printf("~");
 			else if (A[i][j] <= -1.5) printf("_");
 			else if (A[i][j] <= -1.0) printf("*");
@@ -32,12 +32,12 @@ void printScr(double** A, int h, int l) {
 void maths(double** A, int h, int l, float scale, int t, float speed) {
 	for (int i=0; i<h; i++) {
 		for (int j=0; j<l; j++) {
-			A[i][j] = 2*sin(scale*0.5*i + t*speed) * sin(scale*j + t*speed);
-			A[i][j] += sin(scale*1.5*i + t*speed) * sin(scale*2*j + t*speed);
-			A[i][j] += 0.5*sin(scale*4.5*i + t*speed) * sin(scale*4*j + t*speed);
-			A[i][j] += 0.25*sin(scale*13.5*i + t*speed) * sin(scale*8*j + t*speed);
-			A[i][j] += 0.125*sin(scale*40.5*i + t*speed) * sin(scale*16*j + t*speed);
-			A[i][j] += 0.0625*sin(scale*131.5*i + t*speed) * sin(scale*32*j + t*speed);
+			A[i][j] = 2*sin(scale*0.5*i + t*speed) * sin(scale*j + t*speed*4);
+			A[i][j] += sin(scale*1.5*i + t*speed) * sin(scale*2*j + t*speed*4);
+			A[i][j] += 0.5*sin(scale*4.5*i + t*speed) * sin(scale*4*j + t*speed*4);
+			A[i][j] += 0.25*sin(scale*13.5*i + t*speed) * sin(scale*8*j + t*speed*4);
+			A[i][j] += 0.125*sin(scale*40.5*i + t*speed) * sin(scale*16*j + t*speed*4);
+			A[i][j] += 0.0625*sin(scale*131.5*i + t*speed) * sin(scale*32*j + t*speed*4);
 		}
 	}
 }
@@ -59,21 +59,32 @@ int input() {
 }
 
 int main(void) {
-	int h = 50;
-	int l = 100;
+	int h = 69;
+	int l = 246;
 	double** a = (double**)malloc(h*sizeof(double*));
 	for (int i=0; i<h; i++) {
 		a[i] = (double*)malloc(l*sizeof(double));
 	}
 	
-	struct timeval t, t1;
+	
+	struct timeval t, t1, f1, f2;
+	int framerate = 0;
+	int framecount = 0;
+	gettimeofday(&f1, NULL);
 	for (int i=0;;i++) {
+		framecount++;
 		gettimeofday(&t, NULL);
-		maths(a, h, l, 0.075, i, 0.01); 
+		maths(a, h, l, 0.025, i, 0.005); 
 		printScr(a, h, l);
 		usleep(1000);
+		gettimeofday(&f2, NULL);
+		if (f2.tv_sec - f1.tv_sec >= 1) {
+			framerate = framecount;
+			framecount = 0;
+			gettimeofday(&f1, NULL);
+		}
+		//printf("framerate: %d\n", framerate);
 		gettimeofday(&t1, NULL);
-		printf("elapsed usec: %d\n,", t1.tv_usec - t.tv_usec);
 		int diff = 1000000 - (t1.tv_usec - t.tv_usec);
 		diff = diff / 60;
 		usleep(diff);
